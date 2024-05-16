22ab025eca0de415aea68836bcfe5234f2c9b810
ruben <ruskymartinez@gmail.com>
2024-05-16 12:29:36 +0200

    push

--- src/fractol.c
+++ src/fractol.c
@@ -1,7 +1,74 @@
 #include "fractol.h"
+static int	checking_special_chars(char *c)
+{
+	size_t j;
+	int	flag;
 
+	j = 0;
+	flag = 0;
+	if((s[j] == '-' || s[j] == '+') && s[j + 1])	
+		j++;
+	while (j < ft_strlen(s))
+	{
+		if (!ft_isdigit(s[j]))
+		{
+			if (s[j] == '.' && flag == 0 && ft_isdigit(s[j + 1]))
+			{
+				flag = 1;
+				j++;
+			}
+			else
+				return (0);
+		}
+	}
+	j++;
+}
+return (1);
+static int	(char **argv, int n)
+{
+	char *str;
+	double num;
+
+	while(n < 4)
+	{
+		str = argv[n];
+		if (ft_strlen(argv[n]) == 0)
+			return (0);
+		if (checking_special_chars(str) == 0)
+			return (0);
+		num = ft_atod(argv[i], 0, 0.1, 0);
+		if (num < -2 || num > 2)
+			return (0);
+		i++;
+	}
+	return (i);
+}
+int arg_checking(int argc, char **argv)
+{
+	if(!ft_strncmp("julia", argc[1], 6) && argc == 2)
+		return (1);
+	else if(!ft_strncmp("julia", argc[1], 6) && argc == 4)
+	{
+		if(check_avector(argc, 2) == 1)
+			return (1);
+		return (0);
+	}
+	else if (!ft_strncmp("mandelbrot", argv[1], 11) && argc == 2)
+		return (1);
+	else if (!ft_strncmp("tricorn", argv[1], 8) && argc == 2)
+		return (1);
+	return (0);
+
+}
 int main(int argc, char **argv)
 {
+	t_fractal fractol;
 
+	if(argc < 2)
+		return (0);
+	if(arg_checking(argc, argv) == 1)
+	{
+		
+	}
 	return (0);
 }
--- src/fractol.h
+++ src/fractol.h
@@ -7,6 +7,16 @@ typedef struct	s_fractal
 	int	y;
 	long	xy;
 	long	zy;
-
-}
+	void	*mlx;
+	void	*img;
+	void	*win;
+	int	color;
+	double	zoom;
+	char	*name;
+	char	*im_ad;
+	int	endian;
+	int	max_iter;
+	int	line_length;
+	int	bits_per_pixel;
+}	t_fractal;
 #endif
