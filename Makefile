a84c81ecbe2379afc8cbdfccbccb45735cf30b2a
Rubén Medina Martinez <rmedina-@cbr11s2.42barcelona.com>
2024-06-07 19:52:27 +0200

    push

--- Makefile
+++ Makefile
@@ -1,4 +1,34 @@
-##
 NAME = fractol
 HEADER = fractol.h
-LIBS = 
+CC = gcc
+FLAGS = -Wall -Werror -Wextra
+SRCS = /src/fractol.c \
+	   /src/parsing.c
+OBJS = $(SRCS:.c = .o)
+
+%.o : %.c Makefile $(HEADER)
+	$(CC) $(FLAGS) -c $< -o $@
+
+all: $(libs) $(NAME)
+
+$(libs):	
+	make -C /Libs/ft_libft
+	make -C /Libs/ft_printf
+
+$(NAME): $(OBJS)
+	$(CC) $(FLAGS) $(OBJS) -o $(NAME)
+
+clean:
+	rm -f $(OBJS)
+	make -C Libs/ft_libft clean
+	make -C Libs/ft_printf clean
+
+fclean: clean
+	rm -f $(NAME)
+	make -C Libs/ft_libft clean
+	make -C Libs/ft_printf clean
+
+re: fclean all
+
+a
+.PHONY: all clean fclean re $(libs)
--- src/fractol.h
+++ src/fractol.h
@@ -6,7 +6,7 @@
 /*   By: rmedina- <rmedina-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 19:04:30 by rmedina-          #+#    #+#             */
-/*   Updated: 2024/05/31 20:51:41 by rmedina-         ###   ########.fr       */
+/*   Updated: 2024/06/07 16:44:13 by rmedina-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 # include <stdio.h>
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	lower_case_converter(char *str);
+void		lower_case_converter(char *str);
 int		checking_arg(int argc, char **argv);
 int		checking_set(char *str);
 int		ft_isalpha(int c);
-size_t	ft_strlen(const char *c);
-
+size_t		ft_strlen(const char *c);
+int		nbrcompare(int *str);
 #endif
